40559fcf892c61d40dedd4ba61a34011bbeef601
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-08-19 19:39:20 +0200

    FByte and FShort tests

---
+++ tests/Archean.Tests/Networking/FByteTests.cs
@@ -0,0 +1,74 @@
+﻿using Archean.Application.Models.Networking;
+
+namespace Archean.Tests.Networking;
+
+public class FByteTests
+{
+    [Theory]
+    [InlineData(0F, 0F)]
+    [InlineData(0.01F, 0F)]
+    [InlineData(0.04F, 0.03125F)]
+    [InlineData(1F, 1F)]
+    [InlineData(2.5F, 2.5F)]
+    [InlineData(MathF.PI, 3.15625F)]
+    [InlineData(-1F, -1F)]
+    [InlineData(-2.5F, -2.5F)]
+    [InlineData(-MathF.PI, -3.15625F)]
+    public void ConstructFromFloat_WithinRange_ReturnsExpected(float input, float expected)
+    {
+        // Setup
+        FByte fbyte = new FByte(input);
+
+        // Action
+        float actual = fbyte.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(4, FByte.MaxValue)]
+    [InlineData(float.MaxValue, FByte.MaxValue)]
+    [InlineData(float.PositiveInfinity, FByte.MaxValue)]
+    public void ConstructFromFloat_Positive_ReturnsClamped(float input, float expected)
+    {
+        // Setup
+        FByte fbyte = new FByte(input);
+
+        // Action
+        float actual = fbyte.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(-4, FByte.MinValue)]
+    [InlineData(float.MinValue, FByte.MinValue)]
+    [InlineData(float.NegativeInfinity, FByte.MinValue)]
+    public void ConstructFromFloat_Negative_ReturnsClamped(float input, float expected)
+    {
+        // Setup
+        FByte fbyte = new FByte(input);
+
+        // Action
+        float actual = fbyte.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(float.NaN, 0F)]
+    public void ConstructFromFloat_NaN_ReturnsZero(float input, float expected)
+    {
+        // Setup
+        FByte fbyte = new FByte(input);
+
+        // Action
+        float actual = fbyte.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ tests/Archean.Tests/Networking/FShortTests.cs
@@ -0,0 +1,74 @@
+﻿using Archean.Application.Models.Networking;
+
+namespace Archean.Tests.Networking;
+
+public class FShortTests
+{
+    [Theory]
+    [InlineData(0F, 0F)]
+    [InlineData(0.01F, 0F)]
+    [InlineData(0.04F, 0.03125F)]
+    [InlineData(1F, 1F)]
+    [InlineData(200.5F, 200.5F)]
+    [InlineData(MathF.PI, 3.15625F)]
+    [InlineData(-1F, -1F)]
+    [InlineData(-200.5F, -200.5F)]
+    [InlineData(-MathF.PI, -3.15625F)]
+    public void ConstructFromFloat_WithinRange_ReturnsExpected(float input, float expected)
+    {
+        // Setup
+        FShort fshort = new FShort(input);
+
+        // Action
+        float actual = fshort.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(2000, FShort.MaxValue)]
+    [InlineData(float.MaxValue, FShort.MaxValue)]
+    [InlineData(float.PositiveInfinity, FShort.MaxValue)]
+    public void ConstructFromFloat_Positive_ReturnsClamped(float input, float expected)
+    {
+        // Setup
+        FShort fshort = new FShort(input);
+
+        // Action
+        float actual = fshort.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(-2000, FShort.MinValue)]
+    [InlineData(float.MinValue, FShort.MinValue)]
+    [InlineData(float.NegativeInfinity, FShort.MinValue)]
+    public void ConstructFromFloat_Negative_ReturnsClamped(float input, float expected)
+    {
+        // Setup
+        FShort fshort = new FShort(input);
+
+        // Action
+        float actual = fshort.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(float.NaN, 0F)]
+    public void ConstructFromFloat_NaN_ReturnsZero(float input, float expected)
+    {
+        // Setup
+        FShort fshort = new FShort(input);
+
+        // Action
+        float actual = fshort.ToFloat();
+
+        // Assertion
+        Assert.Equal(expected, actual);
+    }
+}
