5aa4c60c008cb87a5d20062d39f7b4c9e64cf712
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-08-19 21:50:45 +0200

    Added blocks

--- src/Archean.Core/Constants.cs
+++ src/Archean.Core/Constants.cs
@@ -1,10 +1,20 @@
-﻿namespace Archean.Core;
+﻿using Archean.Core.Models;
+
+namespace Archean.Core;
 
 /// <summary>
 /// Contains constant values.
 /// </summary>
 public static class Constants
 {
+    public static class Blocks
+    {
+        /// <summary>
+        /// The highest valid Block ID to send to clients.
+        /// </summary>
+        public const Block HighestValidBlockId = Block.Obsidian;
+    }
+
     public static class Networking
     {
         /// <summary>
