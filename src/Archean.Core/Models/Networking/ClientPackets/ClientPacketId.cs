916bd4c8248017df394cebfe45cd308b0b646025
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-08-21 20:04:16 +0200

    Added packet IDs

---
+++ src/Archean.Core/Models/Networking/ClientPackets/ClientPacketId.cs
@@ -0,0 +1,12 @@
+﻿namespace Archean.Core.Models.Networking.ClientPackets;
+
+/// <summary>
+/// Client packet IDs.
+/// </summary>
+public enum ClientPacketId : byte
+{
+    Identification = 0,
+    SetBlock = 5,
+    PositionAndOrientation = 8,
+    Message = 13
+}
---
+++ src/Archean.Core/Models/Networking/ServerPackets/ServerPacketId.cs
@@ -0,0 +1,23 @@
+﻿namespace Archean.Core.Models.Networking.ServerPackets;
+
+/// <summary>
+/// Server packet IDs.
+/// </summary>
+public enum ServerPacketId : byte
+{
+    Identification = 0,
+    Ping = 1,
+    LevelInitialize = 2,
+    LevelDataChunk = 3,
+    LevelFinalize = 4,
+    SetBlock = 6,
+    SpawnPlayer = 7,
+    AbsolutePositionAndOrientation = 8,
+    RelativePositionAndOrientation = 9,
+    RelativePosition = 10,
+    RelativeOrientation = 11,
+    DespawnPlayer = 12,
+    Message = 13,
+    DisconnectPlayer = 14,
+    UpdatePlayerType = 15
+}
