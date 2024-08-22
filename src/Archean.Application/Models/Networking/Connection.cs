11e47a6a8a6223581e1dc03f61763e72c45d9d53
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-08-22 18:23:43 +0200

    Added Connection

---
+++ src/Archean.Application/Models/Networking/Connection.cs
@@ -0,0 +1,36 @@
+﻿using Archean.Core.Models.Networking;
+using System.Net.Sockets;
+
+namespace Archean.Application.Models.Networking;
+
+public class Connection : IConnection
+{
+    private readonly Socket socket;
+
+    public bool IsConnected => socket.Connected;
+
+    public Connection(Socket socket)
+    {
+        this.socket = socket;
+    }
+
+    public async Task<ReadOnlyMemory<byte>> ReadAsync()
+    {
+        Memory<byte> data = new byte[socket.Available];
+        await socket.ReceiveAsync(data);
+        return data;
+    }
+
+    public async Task SendAsync(ReadOnlyMemory<byte> data)
+    {
+        if (IsConnected)
+        {
+            await socket.SendAsync(data);
+        }
+    }
+
+    public void Dispose()
+    {
+        socket.Dispose();
+    }
+}
---
+++ src/Archean.Core/Models/Networking/IConnection.cs
@@ -0,0 +1,10 @@
+﻿namespace Archean.Core.Models.Networking;
+
+public interface IConnection : IDisposable
+{
+    public bool IsConnected { get; }
+
+    public Task SendAsync(ReadOnlyMemory<byte> data);
+
+    public Task<ReadOnlyMemory<byte>> ReadAsync();
+}
