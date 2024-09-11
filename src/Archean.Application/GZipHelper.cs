852e36a2b1a38b3fbe796a1c02772ab827467fa8
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-09-11 17:30:45 +0200

    Added GZipHelper

---
+++ src/Archean.Application/GZipHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.IO.Compression;
+
+namespace Archean.Application;
+
+public static class GZipHelper
+{
+    public static byte[] Compress(ReadOnlySpan<byte> bytes)
+    {
+        using MemoryStream output = new MemoryStream();
+        using (GZipStream gZipStream = new GZipStream(output, CompressionLevel.Optimal))
+        {
+            gZipStream.Write(bytes);
+        }
+
+        return output.ToArray();
+    }
+}
