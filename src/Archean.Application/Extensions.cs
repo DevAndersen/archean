66ec386c94a80a6ee410d190ee188bc1140756af
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-11-18 19:44:24 +0100

    Moved service registrations to AddArcheanDefaultServices extension method

---
+++ src/Archean.Application/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Archean.Application.Services.Networking;
+using Archean.Application.Services;
+using Archean.Core.Services.Networking;
+using Archean.Core.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Archean.Application;
+
+public static class Extensions
+{
+    /// <summary>
+    /// Adds all default Archean services.
+    /// </summary>
+    /// <param name="serviceCollection"></param>
+    /// <returns></returns>
+    public static IServiceCollection AddArcheanDefaultServices(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection
+            .AddScoped<IConnectionHandler, ConnectionHandler>()
+            .AddScoped<IClientPacketReader, ClientPacketReader>()
+            .AddScoped<IServerPacketWriter, ServerPacketWriter>()
+            .AddScoped<IPacketDataReader, PacketDataReader>()
+            .AddScoped<IPacketDataWriter, PacketDataWriter>()
+            .AddScoped<IConnectionService, ConnectionService>()
+            .AddScoped<IClientPacketHandler, ClientPacketHandler>()
+            .AddSingleton<ISocketServer, SocketServer>()
+            .AddSingleton<IConnectionRepository, ConnectionRepository>()
+            .AddSingleton<IBlockDictionary, BlockDictionary>();
+    }
+}
--- src/Archean.Console/Program.cs
+++ src/Archean.Console/Program.cs
@@ -1,22 +1,11 @@
-﻿using Archean.Application.Services;
-using Archean.Application.Services.Networking;
-using Archean.Core.Services;
+﻿using Archean.Application;
 using Archean.Core.Services.Networking;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 ServiceProvider serviceProvider = new ServiceCollection()
     .AddLogging(builder => builder.AddConsole())
-    .AddScoped<IConnectionHandler, ConnectionHandler>()
-    .AddScoped<IClientPacketReader, ClientPacketReader>()
-    .AddScoped<IServerPacketWriter, ServerPacketWriter>()
-    .AddScoped<IPacketDataReader, PacketDataReader>()
-    .AddScoped<IPacketDataWriter, PacketDataWriter>()
-    .AddScoped<IConnectionService, ConnectionService>()
-    .AddScoped<IClientPacketHandler, ClientPacketHandler>()
-    .AddSingleton<ISocketServer, SocketServer>()
-    .AddSingleton<IConnectionRepository, ConnectionRepository>()
-    .AddSingleton<IBlockDictionary, BlockDictionary>()
+    .AddArcheanDefaultServices()
     .BuildServiceProvider();
 
 ISocketServer server = serviceProvider.GetRequiredService<ISocketServer>();
