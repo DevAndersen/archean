a2a80b2ba9ea7e273b4385ecb518c345b72ad65d
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-12-20 02:54:10 +0100

    Added appsettings.json

--- src/Archean.Console/Program.cs
+++ src/Archean.Console/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Archean.Application;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 Host.CreateDefaultBuilder()
+    .ConfigureAppConfiguration(c => c.AddJsonFile("appsettings.json"))
     .ConfigureLogging(l => l.AddConsole())
     .ConfigureServices(s => s.AddArcheanDefaultServices())
     .Build()
