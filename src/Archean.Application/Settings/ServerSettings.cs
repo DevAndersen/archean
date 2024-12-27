285bfe4a0cdce784a22114d31b686c2858076a9a
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2024-12-27 20:10:31 +0100

    Documentation for ServerSettings

--- src/Archean.Application/Settings/ServerSettings.cs
+++ src/Archean.Application/Settings/ServerSettings.cs
@@ -2,13 +2,33 @@
 
 public class ServerSettings
 {
+    /// <summary>
+    /// The port that the socket server runs on.
+    /// </summary>
     public required ushort Port { get; init; } = 25565;
 
+    /// <summary>
+    /// The size of the pending connection backlog.
+    /// </summary>
     public required int Backlog { get; init; } = 3;
 
+    /// <summary>
+    /// The name of the server.
+    /// </summary>
     public required string Name { get; init; }
 
+    /// <summary>
+    /// The MOTD (message of the day) of the server.
+    /// </summary>
     public required string Motd { get; init; }
 
+    /// <summary>
+    /// The MOTD displayed when a player joins the world.
+    /// </summary>
+    /// <remarks>
+    /// <list type="bullet">
+    /// <item>Use "<c>{0}</c>" to show the world loading percentage.</item>
+    /// </list>
+    /// </remarks>
     public required string WorldLoadingMotd { get; init; }
 }
