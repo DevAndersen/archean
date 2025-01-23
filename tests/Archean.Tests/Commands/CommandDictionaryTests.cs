00f53004efb934cdd5425c7513806d20ce545368
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-01-24 00:05:44 +0100

    Added CommandDictionaryTests

---
+++ tests/Archean.Tests/Commands/CommandDictionaryTests.cs
@@ -0,0 +1,119 @@
+﻿using Archean.Application.Services.Commands;
+using Archean.Core.Models.Commands;
+using Archean.Core.Services.Commands;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+
+namespace Archean.Tests.Commands;
+
+public class CommandDictionaryTests
+{
+    private readonly ILogger<CommandDictionary> mockedLogger;
+    private readonly IServiceProvider mockedServiceProvider;
+
+    public CommandDictionaryTests()
+    {
+        mockedLogger = Substitute.For<ILogger<CommandDictionary>>();
+        mockedServiceProvider = Substitute.For<IServiceProvider>();
+    }
+
+    [Fact]
+    public void Register_EmptyDictionary_ReturnsTrue()
+    {
+        // Setup
+        ICommandDictionary commandDictionary = new CommandDictionary(mockedLogger, mockedServiceProvider);
+
+        // Action
+        bool wasCommandRegistered = commandDictionary.Register<TestCommand>();
+
+        // Assert
+        Assert.True(wasCommandRegistered);
+    }
+
+    [Fact]
+    public void Register_CommandAlreadyInDictionary_ReturnsFalse()
+    {
+        // Setup
+        ICommandDictionary commandDictionary = new CommandDictionary(mockedLogger, mockedServiceProvider);
+
+        // Action
+        commandDictionary.Register<TestCommand>();
+        bool wasCommandRegistered = commandDictionary.Register<TestCommand>();
+
+        // Assert
+        Assert.False(wasCommandRegistered);
+    }
+
+    [Fact]
+    public void TryGetCommand_CommandName_CommandFound()
+    {
+        // Setup
+        mockedServiceProvider.GetService(typeof(TestCommand)).Returns(new TestCommand());
+        ICommandDictionary commandDictionary = new CommandDictionary(mockedLogger, mockedServiceProvider);
+
+        // Action
+        commandDictionary.Register<TestCommand>();
+        bool wasCommandFound = commandDictionary.TryGetCommand(TestCommand.CommandName, out ICommand? command);
+
+        // Assert
+        Assert.True(wasCommandFound);
+        Assert.True(command is TestCommand);
+    }
+
+    [Fact]
+    public void TryGetCommand_CommandAliases_CommandFound()
+    {
+        // Setup
+        mockedServiceProvider.GetService(typeof(TestCommand)).Returns(new TestCommand());
+        ICommandDictionary commandDictionary = new CommandDictionary(mockedLogger, mockedServiceProvider);
+
+        // Action
+        commandDictionary.Register<TestCommand>();
+        bool foundByAlias1 = commandDictionary.TryGetCommand(TestCommand.CommandAliases[0], out ICommand? commandByAlias1);
+        bool foundByAlias2 = commandDictionary.TryGetCommand(TestCommand.CommandAliases[1], out ICommand? commandByAlias2);
+
+        // Assert
+        Assert.True(foundByAlias1);
+        Assert.True(foundByAlias2);
+        Assert.True(commandByAlias1 is TestCommand);
+        Assert.True(commandByAlias2 is TestCommand);
+    }
+
+    [Fact]
+    public void TryGetCommand_IncorrectName_CommandNotFound()
+    {
+        // Setup
+        ICommandDictionary commandDictionary = new CommandDictionary(mockedLogger, mockedServiceProvider);
+
+        // Action
+        bool wasCommandFound = commandDictionary.TryGetCommand(TestCommand.CommandName, out ICommand? command);
+
+        // Assert
+        Assert.False(wasCommandFound);
+        Assert.Null(command);
+    }
+
+    [Fact]
+    public void TryGetCommand_CommandNotInServiceProvider_CommandFound()
+    {
+        // Setup
+        ICommandDictionary commandDictionary = new CommandDictionary(mockedLogger, mockedServiceProvider);
+
+        // Action
+        commandDictionary.Register<TestCommand>();
+        bool wasCommandFound = commandDictionary.TryGetCommand(TestCommand.CommandName, out ICommand? command);
+
+        // Assert
+        Assert.False(wasCommandFound);
+        Assert.Null(command);
+    }
+
+    private class TestCommand : ICommand
+    {
+        public static string CommandName => nameof(TestCommand);
+
+        public static string[] CommandAliases => ["t", "123"];
+
+        public Task InvokeAsync() => Task.CompletedTask;
+    }
+}
