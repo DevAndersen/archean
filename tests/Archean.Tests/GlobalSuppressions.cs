b42f49457723e773d9e75202f9e4c3e6b6860f30
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-05-23 20:47:53 +0200

    Added GlobalSuppressions

---
+++ tests/Archean.Tests/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage(
+    "Performance",
+    "CA1859:Use concrete types when possible for improved performance",
+    Justification = "Replicating expected behavior is more important than performance for a test project.")]
