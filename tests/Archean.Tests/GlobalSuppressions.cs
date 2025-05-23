9d3c1039a9a33029198010117ee8c6feceab8ac6
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-05-23 20:55:08 +0200

    Added IDE0039 to test project GlobalSuppressions

--- tests/Archean.Tests/GlobalSuppressions.cs
+++ tests/Archean.Tests/GlobalSuppressions.cs
@@ -3,4 +3,9 @@
 [assembly: SuppressMessage(
     "Performance",
     "CA1859:Use concrete types when possible for improved performance",
-    Justification = "Replicating expected behavior is more important than performance for a test project.")]
+    Justification = "Replicating expected behavior is more important than improving performance for a test project.")]
+
+[assembly: SuppressMessage(
+    "Style",
+    "IDE0039:Use local function",
+    Justification = "It makes certain tests simpler to write.")]
