4dc9d8a10b18ae6bc09bdb8b7f14f6312971a623
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-07-28 10:11:26 +0200

    Added documentation for ChatPage.ChatColorRegex

--- src/Archean.App.WebApp/Components/Pages/ChatPage.razor.cs
+++ src/Archean.App.WebApp/Components/Pages/ChatPage.razor.cs
@@ -4,6 +4,16 @@ namespace Archean.App.WebApp.Components.Pages;
 
 public partial class ChatPage
 {
+    /// <summary>
+    /// Regex pattern for grouping text according to coloring, defined as an ampersand (&amp;) followed by a single hexadecimal character.
+    /// </summary>
+    /// <remarks>
+    /// <list type="bullet">
+    /// <item>Match group 1: The ampersand and hexadecimal character, indicating which color to be used. May be undefined.</item>
+    /// <item>Match group 2: The text that should use the color defined in match group 1.</item>
+    /// </list>
+    /// </remarks>
+    /// <returns></returns>
     [GeneratedRegex("(&[0-9a-f]|^)?(.+?)(?=&|$)", RegexOptions.IgnoreCase)]
     private static partial Regex ChatColorRegex();
 }
