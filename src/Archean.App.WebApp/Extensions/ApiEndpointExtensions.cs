618741f502976f87798e7dde80cf3090123a5f5c
DevAndersen <25183991+DevAndersen@users.noreply.github.com>
2025-08-03 01:21:30 +0200

    Added input validation to skin endpoint

--- src/Archean.App.WebApp/Extensions/ApiEndpointExtensions.cs
+++ src/Archean.App.WebApp/Extensions/ApiEndpointExtensions.cs
@@ -8,9 +8,24 @@ public static class ApiEndpointExtensions
     {
         return builder.MapGet("skin/{skin}", ([FromRoute] string skin) =>
         {
+            // Check for illegal sequences.
+            if (skin.Contains("..") || skin.Contains('/') || skin.Contains('\\'))
+            {
+                return TypedResults.NotFound();
+            }
+
             string skinsDirectory = "skins"; // Todo: Get value from configuration.
             string skinPath = Path.Combine(skinsDirectory, skin);
 
+            // Check for paths outside of skin directory.
+            string absoluteSkinPath = Path.GetFullPath(skinPath);
+            string absoluteSkinDirectory = Path.GetFullPath(skinsDirectory);
+            if (!absoluteSkinPath.StartsWith(absoluteSkinDirectory))
+            {
+                return TypedResults.NotFound();
+            }
+
+            // Check if the requested file exists.
             if (!File.Exists(skinPath))
             {
                 return TypedResults.NotFound();
